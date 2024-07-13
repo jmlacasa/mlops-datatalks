1e597aa80735c8938af68734169c3d4133fce656
jmlacasa <61094100+jmlacasa@users.noreply.github.com>
2024-07-13 20:44:07 -0300

    worked on hyperparameter optimization with mlflow

--- 02-experiment-tracking/hw/train.py
+++ 02-experiment-tracking/hw/train.py
@@ -7,9 +7,10 @@ from sklearn.metrics import mean_squared_error
 
 import mlflow
 
-mlflow.set_tracking_uri("sqlite:///mlflow.db")
+mlflow.set_tracking_uri("http://127.0.0.1:5000")
 mlflow.set_experiment("nyc-taxi-experiment")
 mlflow.autolog()
+
 
 def load_pickle(filename: str):
     with open(filename, "rb") as f_in:
@@ -25,10 +26,13 @@ def load_pickle(filename: str):
 def run_train(data_path: str):
     
     with mlflow.start_run():
+        print(mlflow.get_tracking_uri())
+        print(mlflow.get_artifact_uri())
         X_train, y_train = load_pickle(os.path.join(data_path, "train.pkl"))
         X_val, y_val = load_pickle(os.path.join(data_path, "val.pkl"))
 
         rf = RandomForestRegressor(max_depth=10, random_state=0)
+        # mlflow.sklearn.log_model(rf, artifact_path="model")
         rf.fit(X_train, y_train)
         y_pred = rf.predict(X_val)
 
